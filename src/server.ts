f849d8079f4cc90eb510d1d1bb400718dd38a1d2
Felipe300 <caracoloro@gmail.com>
2022-06-12 21:58:10 -0400

    fix: Add eslint

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,22 @@ import cors from 'cors'
 
 class ServerBoostrap {
   public app: express.Application = express()
-  private port: number = 5000
+  private port: number = 8000
 
-  constructor() {
+  constructor () {
     this.app.use(express.json())
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(morgan('dev'))
     this.app.use(cors())
+
+    this.app.get('/api/test', (req, res) => {
+      res.status(200).json({ message: 'Hello World!!!' })
+    })
+
     this.listen()
   }
 
-  public listen() {
+  public listen () {
     this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port}`)
     })
